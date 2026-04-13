dbe4d27d6b769c915e4fc534ce0857dbf3b3b4cf
Leela Mahalakshmi <leela592023@gmail.com>
2026-04-13 15:14:49 +0530

    Implement merge intervals function

---
+++ Arrays_Hashing/56.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int[][] merge(int[][] intervals) {
+        ArrayList<int[]> res = new ArrayList<>();
+        Arrays.sort(intervals,(a,b)->a[0]-b[0]);
+        int curr[] = intervals[0];
+        for(int i=1;i<intervals.length;i++){
+            if(intervals[i][0] <= curr[1]){
+                curr[1] = Math.max(curr[1],intervals[i][1]);
+            }
+            else{
+                res.add(curr);
+                curr = intervals[i];
+            }
+        }
+        res.add(curr);
+        return res.toArray(new int[res.size()][]);
+    }
+}
