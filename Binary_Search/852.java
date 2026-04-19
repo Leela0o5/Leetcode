239465597208479ff78a7387824a60961d0bd1d7
Leela Mahalakshmi <leela592023@gmail.com>
2026-04-19 21:09:28 +0530

    Implement peak index finder in mountain array

---
+++ Binary_Search/852.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int peakIndexInMountainArray(int[] arr) {
+        int s = 0;
+        int e = arr.length - 1;
+        int m = s+(e-s)/2;
+        while(s<e){
+            if(arr[m+1]>arr[m]) s = m+1;
+            else e = m;
+            m = s+(e-s)/2;
+        }
+        return s;
+    }
+}
