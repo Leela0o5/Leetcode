9fc275639f3b38a5ad44b62785d1ca9acfcfb4c7
Leela Mahalakshmi <leela592023@gmail.com>
2026-05-14 20:52:50 +0530

    Implement Sudoku solver using backtracking

---
+++ Recursion/37.java
@@ -0,0 +1,44 @@
+class Solution {
+    public boolean isSafe(char[][] board,int row,int col,char dig){
+        for(int j=0;j<9;j++){
+            if(board[row][j] == dig)  return false;
+        }
+        // vertical
+        for(int i=0;i<9;i++){
+            if(board[i][col] == dig)  return false;
+        }
+
+        int sr = (row/3)*3;
+        int sc = (col/3)*3;
+
+        // in the grid
+        for(int i=sr;i<=sr+2;i++){
+            for(int j=sc;j<=sc+2;j++){
+                if(board[i][j]==dig) return false;
+            }
+        }
+        return true;
+    }
+    public boolean helper(char[][] board,int row,int col){
+        if(row == 9 ) return true;
+           int nextRow = row;
+           int nextCol = col + 1;
+           if(nextCol == 9){nextRow = row+1;nextCol=0;}
+           if(board[row][col] !='.'){
+                    return helper(board,nextRow,nextCol);
+           }
+           for(char dig = '1';dig<='9';dig++){
+            if(isSafe(board,row,col,dig)){
+                board[row][col] = dig;
+                if(helper(board,nextRow,nextCol)) return true;
+            }
+            // Backtrack
+            board[row][col] = '.';
+           }
+
+           return false;
+    }
+    public void solveSudoku(char[][] board) {
+         helper(board,0,0);  
+    } 
+}
